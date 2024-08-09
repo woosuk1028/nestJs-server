9ae1e0862647a6f3cec4dbd6dcaa4d548a05a2ed
WooSeok <gmn94@naver.com>
2024-08-09 10:45:07 +0900

    20240809 first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'https://seok2.duckdns.org', // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
+  await app.listen(3001);
 }
 bootstrap();
